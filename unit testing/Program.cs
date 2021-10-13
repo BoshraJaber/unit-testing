9a47b5c2af9ea71fed5e4742728ee4741d961798
Boshra Husein Jaber <b.Jaber@ltuc.com>
2021-10-13 19:47:07 +0300

    User interface

--- unit testing/Program.cs
+++ unit testing/Program.cs
@@ -4,9 +4,58 @@ namespace ATM
 {
     class Program
     {
+        // Global variable 
+        static public decimal Balance = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserInterface();
+        }
+
+        // user interface:
+
+          public static void UserInterface()
+        {
+            Boolean showInterface = true;
+            Console.WriteLine("Hello to our ATM, Kindly enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Please select one of the options  {userName}:");
+            Console.WriteLine("Press 1 to view your Balance");
+            Console.WriteLine("Press 2 to Deposit");
+            Console.WriteLine("Press 3 to withdraw");
+            Console.WriteLine("Press 4 to Exit");
+
+            while (showInterface)
+            {
+              
+                string userInput = Console.ReadLine();
+
+                if(userInput == "1")
+                {
+                    //Console.WriteLine(ViewBalance());
+                }
+                else if(userInput == "3")
+                {
+                    Console.Write("Please enter amount of money > ");
+                    string money = Console.ReadLine();
+                    //Console.WriteLine(Withdraw(Convert.ToDecimal(money)));
+                }
+                else if (userInput == "2")
+                {
+                    Console.Write("Please enter amount of money > ");
+                    string money = Console.ReadLine();
+                 //   Console.WriteLine(Deposit(Convert.ToDecimal(money)));
+                }
+                else if(userInput == "4")
+                {
+                    Console.WriteLine("Thank you");
+                    showInterface = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+
+            }
         }
     }
 }
