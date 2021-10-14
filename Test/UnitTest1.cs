dc7b616e62c01bb9673080e466b2bd32c1d5adb3
Boshra Husein Jaber <b.Jaber@ltuc.com>
2021-10-14 08:59:14 +0300

    testing project

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using ATM;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Program.ViewBalance();
+
+        }
+    }
+}
